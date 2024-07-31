708c81249faeadba34db9eedef8596881926edef
Maaj <maajb1122@gmail.com>
2024-07-31 22:16:32 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Benefits from './components/Benefits';
 import Button from './components/Button'
+import Collaboration from './components/Collaboration';
 import Header from './components/Header';
 import Hero from './components/Hero';
  const App=()=> {
@@ -13,6 +14,7 @@ import Hero from './components/Hero';
       <Header/>
       <Hero/>
       <Benefits/>
+      <Collaboration/>
       </div>
       <ButtonGradient/>
     </>
