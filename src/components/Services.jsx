b0985d75061a10ab6627e27fbaef72aac58f213f
Maaj <maajb1122@gmail.com>
2024-08-01 20:31:37 +0530

    committed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Section from './Section';
 import Heading from './Heading';
-import { check, service1 } from '../assets';
+import { check, service1, service2 } from '../assets';
 import { brainwaveServices } from '../constants';
+import {PhotChatMessage,Gradient,VideoBar,VideoChatMessage} from './design/Services'
 import Generating from './Generating';
 
 const Services = () => {
@@ -38,6 +39,18 @@ const Services = () => {
               </ul>
             </div>
             <Generating className='absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg:right-auto lg:bottom-8 lg:-translate-x-1/2'/>
+          </div>
+          <div className='relative z-1 grid gap-5 lg:grid-cols-2'>
+            <div className='relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden'>
+                <div className='absolute inset-0'>
+                  <img src={service2} alt="robot" className='h-full w-full object-cover' width={630} height={750} />
+                </div>
+                <div className='absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b-from-n-8/90 to-n-8/90 lg:p-15'>
+              <h4 className='h4 mb-4'>Photo Editing</h4>
+              <p className='body-2 mb-[3rem] text-n-3'>Automatically enhance your photos using our AI app photo editing feature. Try it now</p>
+              </div>
+              <PhotChatMessage/>
+            </div>
           </div>
         </div>
       </div>
