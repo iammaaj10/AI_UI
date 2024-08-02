c01453a81d988969e2c7ade7a52ce30dcc2975ce
Maaj <maajb1122@gmail.com>
2024-08-02 19:10:45 +0530

    committed

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Section from './Section'
 import { smallSphere, stars } from '../assets'
 import Heading from './Heading'
+import PricingList from './PricingList'
+import {LeftLine,RightLine} from './design/Pricing'
 
 const Pricing = () => {
   return (
@@ -17,6 +19,12 @@ const Pricing = () => {
         tag="Get Started with Brainwave"
         title="Pay once, use forever"
         />
+        <div className='relative '>
+            <PricingList/>
+            <LeftLine/>
+            <RightLine/>
+            
+        </div>
     </div>
 
    </Section>
